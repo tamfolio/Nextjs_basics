e8544b483fa87aca30a2822209f1b0d21b999055
unknown <tamiloreolaniyi@gmail.com>
2022-03-25 05:37:31 +0100

    Layout and styling

---
+++ components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../styles/Layout.module.css'
+
+function Layout({ children}) {
+  return (
+    <div className={styles.container}>
+        <main className={styles.main}>
+            {children}
+        </main>
+
+    </div>
+  )
+}
+
+export default Layout
